3175c49fe587ab892fa2a68b1f14f90ad5dcc22b
Konstantinos Peratinos <kotsosgr@gmail.com>
2017-09-09 15:19:07 +0200

    Removed temp files, added 2 new semaphores for crossin, adjusted 2 sensor locations

--- Assign1 -Trainspotting/src/Lab1.java
+++ Assign1 -Trainspotting/src/Lab1.java
@@ -6,7 +6,8 @@ public class Lab1 {
 	Train t1, t2;
 	private final Semaphore turnS = new Semaphore(1); // South turn
 	private final Semaphore turnM = new Semaphore(1); // Middle Turn
-	private final Semaphore turnN = new Semaphore(1); // North cROSSING
+	private final Semaphore CrossN = new Semaphore(1); // North cROSSING
+    private final Semaphore CrossS = new Semaphore(1); // South cROSSING
 
 	public Lab1(Integer speed1, Integer speed2) {
 
@@ -38,9 +39,15 @@ public class Lab1 {
 			SensorEvent t1Event;
 			String xy = new String();
 			boolean dir;
-			
-			if (id == 1 ) {dir = true;} // Set the direction of the train true = top to bottom
-			else {dir = false;}
+
+			if (id == 1 ) { // Set the direction of the train true = top to bottom
+                dir = true;
+                CrossN.acquire(); // Assigning South semaphore initially to N train
+                }
+			else {
+                dir = false;
+                 CrossS.acquire(); // Assigning South semaphore initially to S train
+                }
 			try {
 				tsim.setSpeed(id, speed);
 				while(true){
@@ -52,54 +59,95 @@ public class Lab1 {
 					else { xy = "0";}
 					// Switch on different sensors
 					switch (xy) {
-						case "163":
-						case "165":
+						case "153":
+						case "155":
 							if(dir == false){
 								tsim.setSpeed(id, 0);
 								Thread.sleep(1000 + (20 * speed));
 								tsim.setSpeed(id, -speed);
 								dir = true;
-								}	
+								}
 								break;
-						case "1611":
-						case "1613":
+						case "1511":
+						case "1513":
 							if(dir == true){
 							tsim.setSpeed(id, 0);
 							Thread.sleep(1000 + (20 * speed));
 							tsim.setSpeed(id, -speed);
 							dir = false;
-							}	
+							}
 							break;
 						case "158":
+                        if(dir == true) {
+                                if (turnM.tryAcquire()) {
+                                    tsim.setSwitch(17,7,1); // 1 = Switch left
+                                }
+                                else{
+                                tsim.setSpeed(id, 0);
+                                turnM.acquire();
+                                tsim.setSpeed(id, speed);
+                                tsim.setSwitch(17,7,1);
+                                }
+                            }
+                            else{
+                                turnM.release();
+                            }
+							break;
 						case "157":
-							if(dir == true) { tsim.setSwitch(17,7,2); } // 2 = Switch right
+                            if(dir == true) {
+                                if (turnM.tryAcquire()) {
+                                    tsim.setSwitch(17,7,2); // 2 = Switch right
+                                }
+                                else{
+                                tsim.setSpeed(id, 0);
+                                turnM.acquire();
+                                tsim.setSpeed(id, speed);
+                                tsim.setSwitch(17,7,2);
+                                }
+                            }
+                            else{
+                                turnM.release();
+                            }
 							break;
 							// NEED SEMAPHORE MIDDLE TURN
 						case "139":
 						case "1310":
-							if(dir == false) { tsim.setSwitch(15,9,2); } // 2 = Switch right
+
+							if(dir == false) {
+                                if (turnM.tryAcquire()) {
+                                tsim.setSwitch(15,9,2); // 2 = Switch right
+                                }
+                                else{
+                                tsim.setSpeed(id, 0);
+                                turnM.acquire();
+                                tsim.setSpeed(id, speed);
+                                tsim.setSwitch(15,9,2);
+                                }
+                            }
+                            else{
+                                turnM.release();
+                            }
 							break;
 							// NEED SEMAPHORE MIDDLE TURN
-						case "197":
-							// Need a semaphore and Switch'
-						case "85":
 						case "67":
 						case "107":
 						case "108":
 							// nEED SEMAPHORE NORTH CROSSING
 						case "197":
-							// SEMAPHORE UPPER RIGHT CROSSING -- DIRECTION FALSE
+							// SEMAPHORE UPPER RIGHT CROSSING  and a switch-- DIRECTION FALSE
+                            if(dir == false) { tsim.setSwitch(17,7,1); } // 2 = Switch right
+							break;
 						case "179":
 							// SEMAPHOERE MIDLE RIGHT CROSSING -- DIRECTION TRUE
 						case "29":
 							// SEMAPHORE MIDLE LEFT CROSSING -- DIRECTION FALSE
 						case "111":
-							// SEMAPHORE LOWER CROSSING -- DIRECTION TRUE
+							// SEMAPHORE SOUTH CROSSING -- DIRECTION TRUE
 						default:
 					}
 				}
 			}
-			 
+
 			catch (Exception e) {
 				e.printStackTrace(); // or only e.getMessage() for the error
 				System.exit(1);
